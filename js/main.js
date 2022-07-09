ef0da6d1a6033aaae276b340881be0937df96058
Angela Vegega <angela.vegega@gmail.com>
2022-07-10 02:30:41 +0900

    populated dropdown box

--- js/main.js
+++ js/main.js
@@ -1,38 +1,42 @@
-const RESTAURANT_URL = `https://nanitabeyouka.herokuapp.com/api/${value}`
-document.getElementById('selectFoodType').addEventListener('onChange', apiRequest)
+const RESTAURANT_URL = `https://nanitabeyouka.herokuapp.com/api`
+const select = document.querySelector('#selectFoodType')
+// document.getElementById('selectFoodType').addEventListener('onChange', apiRequest)
 
-function apiRequest() {
-  const foodType = document.getElementById('selectFoodType')
-  const option = foodType.options[foodType.selectedIndex]
+fetch(RESTAURANT_URL)
+  .then(res => {
+    return res.json()
+  })
+  .then(data => {
+    console.log(data.pizza[0].name);
+    console.log(data);
+    let foodKeyArray = Object.keys(data)
+    console.log(foodKeyArray);
 
-  console.log(option.value);
-  console.log(option.text);
-}
-
-apiRequest()
-
-// async function apiRequest() {
-  
-//   try{
-//     const foodType = document.querySelector('select')
-//   const value = foodType.options[foodType.selectedIndex]
-//     const response = await fetch(`https://nanitabeyouka.herokuapp.com/api/${value}`)
-//     const data = await response.json()
-// console.log(value)
-//     console.log(data);
+    for(let i = 0; i < foodKeyArray.length; i++) {
+      const option = document.createElement('option')
+      option.value = foodKeyArray[i]
+      option.innerText = foodKeyArray[i]
+      select.appendChild(option)
+    }
+  })
     
-//     for (let key of data) {
-//       console.log(key.name);
-//       const nameEle = document.createElement('h1')
-//       nameEle.innerText = `Restaraunt name: ${key.name}`
-//       document.body.appendChild(nameEle)
-//       const areaEle = document.createElement('h1')
-//       areaEle.innerText = `Area the restaurant is in: ${key.area}`
-//       document.body.appendChild(areaEle)
-//     }
-
+// async function apiRequest() {
+  // try{
+  //   const value = foodType.options[foodType.selectedIndex]
+  //   const response = await fetch(RESTAURANT_URL)
+  //   const data = await response.json()
+  //   console.log(data);
+    
+    // for (let key of data) {
+    //   console.log(key.name);
+    //   const nameEle = document.createElement('h1')
+    //   nameEle.innerText = `Restaraunt name: ${key.name}`
+    //   document.body.appendChild(nameEle)
+    //   const areaEle = document.createElement('h1')
+    //   areaEle.innerText = `Area the restaurant is in: ${key.area}`
+    //   document.body.appendChild(areaEle)
+    // }
     // console.log(restaurantName);
-
     // document.querySelector('.name').innerText = restaurantName
 //   }catch(error){
 //     console.log(error);
