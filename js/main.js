3aa1d6333dcc44b3b7d2a23bdf0aac67240c363e
Angela Vegega <angela.vegega@gmail.com>
2022-07-12 21:16:54 +0900

    loop through choice and display to the DOM

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ fetch(RESTAURANT_URL)
     return res.json()
   })
   .then(data => {
-    console.log(data.pizza[0].name);
-    console.log(data);
+    // console.log(data.pizza[0].name);
+    // console.log(data);
     let foodKeyArray = Object.keys(data)
-    console.log(foodKeyArray);
+    // console.log(foodKeyArray);
 
     for(let i = 0; i < foodKeyArray.length; i++) {
       const option = document.createElement('option')
@@ -19,6 +19,29 @@ fetch(RESTAURANT_URL)
       select.appendChild(option)
     }
   })
+
+  select.addEventListener('change', event => {
+    console.log(event.target.value);
+    let url = `https://nanitabeyouka.herokuapp.com/api/${event.target.value}`
+    getRestaurant(url)
+  })
+
+  const getRestaurant = url => {
+    fetch(url)
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        const newElement = document.createElement('div')
+        newElement.setAttribute('class', 'new-div')
+        newElement.innerHTML = `<h1>${data[i].name}</h1>
+        <h2>${data[i].area}</h2>`
+        document.body.appendChild(newElement)
+      }
+    })
+  }
     
 // async function apiRequest() {
   // try{
